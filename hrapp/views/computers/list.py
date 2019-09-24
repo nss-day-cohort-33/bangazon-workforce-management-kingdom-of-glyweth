c9dfe389bf51e04e1fb3208726c8c3d712e83ac7
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-09-24 11:53:48 -0500

    views for computer is working

--- hrapp/views/computers/list.py
+++ hrapp/views/computers/list.py
@@ -1,12 +1,12 @@
 import sqlite3
 from django.shortcuts import render
-from django.shortcuts import redirect
-from django.urls import reverse
+# from django.shortcuts import redirect
+# from django.urls import reverse
 from hrapp.models import Computer
 from ..connections import Connection
 
-def computer_list(rcdequest):
-    if request.method == "GET":
+def computer_list(request):
+    # if request.method == "GET":
         with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
