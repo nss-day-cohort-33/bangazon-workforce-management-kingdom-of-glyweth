4b2ba84f378bad7bb5e0f78476ee81d44541677c
berkeley platte <berkeleyplatte989@gmail.com>
2019-09-24 12:07:25 -0500

    updated .gitignore and connection path

--- hrapp/views/employees/employee_list.py
+++ hrapp/views/employees/employee_list.py
@@ -2,11 +2,12 @@ import sqlite3
 from django.shortcuts import render
 from hrapp.models import Employee
 from django.contrib.auth.decorators import login_required
+from ..connections import Connection
 
 @login_required
 def employee_list(request):
     if request.method == 'GET':
-        with sqlite3.connect("/Users/platt/workspace/python/bangazon-workforce-management-kingdom-of-glyweth/db.sqlite3") as conn:
+        with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
             db_cursor = conn.cursor()
 
