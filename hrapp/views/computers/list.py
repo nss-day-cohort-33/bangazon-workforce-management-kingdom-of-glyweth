81f31d96d6dfea14e2d7e4419254f53d97e8fb30
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-09-24 10:58:24 -0500

    initial commit on computer list

---
+++ hrapp/views/computers/list.py
@@ -0,0 +1,41 @@
+import sqlite3
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
+from hrapp.models import Computer
+from ..connections import Connection
+
+def computer_list(rcdequest):
+    if request.method == "GET":
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select
+				c.manufacturer,
+				c.model,
+				c.purchase_date,
+				c.decommission_date
+            from hrapp_computer c
+            """)
+
+            all_computers = []
+            dataset = db_cursor.fetchall()
+
+            for row in dataset:
+                computer = Computer()
+                computer.manufacturer = row['manufacturer']
+                computer.model = row['model']
+                computer.purchase_date = row['purchase_date']
+                computer.decommission_date = row['decommission_date']
+
+                all_computers.append(computer)
+
+
+        template = 'computers/list.html'
+        context = {
+            'computers': all_computers
+        }
+        #In DJANGO you have to manually wire up URLs
+        return render(request, template, context)
--- hrapp/views/connections.py
+++ hrapp/views/connections.py
@@ -1,2 +1,2 @@
 class Connection:
-    db_path = ""
+    db_path = "/Users/stuff/workspace/python/bangazon-workforce-management-kingdom-of-glyweth/db.sqlite3"
