cd3c08368e2eae285af558469caa93604eeb6220
MelanieJaneBond <miss.bond2@gmail.com>
2019-09-25 15:32:32 -0500

    started trying to list employees by name

--- hrapp/views/departments/details.py
+++ hrapp/views/departments/details.py
@@ -25,6 +25,26 @@ def get_department(department_id):
 
         return db_cursor.fetchone()
 
+
+        # all_departments = []
+        #     dataset = db_cursor.fetchall()
+
+        #     for row in dataset:
+        #         dep = Department()
+        #         dep.id = row["id"]
+        #         dep.employee_count = row["employee_count"]
+        #         dep.name = row["name"]
+        #         dep.budget = row["budget"]
+
+        #         all_departments.append(dep)
+    #MELANIE! Don't panic... you need to figure out whether you want to get all the employees
+    # or just get one table that is for the one department ID.
+    #In the SQL call, when I specified "1" as the dep.id, it showed me a table of all the employees
+    # that go with dep.id = 1.
+    #So this SQL call is bringing in more than one value but it is still only fetching the ID of
+    # one department.
+    #I may need advice from Stack Overflow or from Joe and Steve...
+
 # @login_required
 def department_details(request, department_id):
     if request.method == 'GET':
