e4dc69f05cc2d5fc2f1c914a95db8a608cd1a9b6
joekennerly <joseph.kennerly@gmail.com>
2019-09-25 15:00:59 -0500

    can delete

--- hrapp/views/training/details.py
+++ hrapp/views/training/details.py
@@ -36,3 +36,20 @@ def training_details(request, training_id):
         }
 
         return render(request, template, context)
+    if request.method == 'POST':
+        form_data = request.POST
+
+        # Check if this POST is for deleting
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "DELETE"
+        ):
+            with sqlite3.connect(Connection.db_path) as conn:
+                db_cursor = conn.cursor()
+
+                db_cursor.execute("""
+                DELETE FROM hrapp_training_program
+                WHERE id = ?
+                """, (training_id,))
+
+            return redirect(reverse('hrapp:training_list'))
